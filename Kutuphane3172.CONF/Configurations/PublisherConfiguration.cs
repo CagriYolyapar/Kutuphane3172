5eb89f3684f1845861beece475e286f48ba543d4
Bora Mizrahi Mitrani <boramizrahi3@gmail.com>
2024-08-14 13:58:11 +0300

    Conf katmaninda, Configuration dosyasinin icined PublisherConfiguration class'i olusturuldu ve Constructor'u default olarak acildi.

---
+++ Kutuphane3172.CONF/Configurations/PublisherConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Kutuphane3172.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Kutuphane3172.CONF.Configurations
+{
+    public class PublisherConfiguration : BaseConfiguration<Publisher>
+    {
+        public PublisherConfiguration()
+        {
+
+        }
+    }
+}
