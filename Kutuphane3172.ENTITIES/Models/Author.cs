2ed07ea941ca79f5d1cb57addcde5d4110e93b89
cagri.yolyapar <cagri.yolyapar@gmail.com>
2024-08-14 14:04:47 +0300

    Author class'ı icerisinde Deneme isminde bir yorum satırı yazıldı

--- Kutuphane3172.ENTITIES/Models/Author.cs
+++ Kutuphane3172.ENTITIES/Models/Author.cs
@@ -11,6 +11,8 @@ namespace Kutuphane3172.ENTITIES.Models
         public string UserName { get; set; }
         public string  Password { get; set; }
 
+        //Deneme
+
         //Relational Properties
         public virtual AuthorProfile Profile { get; set; }
         public virtual List<Book> Books { get; set; }
